e4ef326b76c42f178ef566a53ee05894711c2505
Yeondam Park <giyen9088@gmail.com>
2019-04-29 21:37:42 -0700

    setting up routes

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect Database
+/**
+ * Connecting to Database
+ */
 connectDB();
 
 app.get("/", (req, res) => res.send("hello"));
+
+/**
+ * Define Routes
+ */
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
