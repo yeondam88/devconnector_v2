dc13dab9ca7852562257552cdf42099ac76e5d40
Yeondam Park <giyen9088@gmail.com>
2019-04-29 21:53:02 -0700

    Registering a user

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
  */
 connectDB();
 
+/**
+ * Init Middleware
+ */
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("hello"));
 
 /**
