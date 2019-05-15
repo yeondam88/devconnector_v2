5155941e0b7fd0bc0f85bed999db27443ee57e70
Yeondam Park <giyen9088@gmail.com>
2019-05-14 21:26:00 -0700

    Adding post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,14 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
 
+const Post = require("../../model/Post");
+const User = require("../../model/User");
+const Profile = require("../../model/Profile");
 /**
  * @route       GET api/posts
- * @description Test route
- * @access      Public
+ * @description Create a post
+ * @access      Private
  */
 
-router.get("/", (req, res) => {
-  res.send("posts route");
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is Required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
